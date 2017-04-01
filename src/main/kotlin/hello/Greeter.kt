f7b08cf6a0d8fc2cea3a18e1677c050a36dc371e
Mark Drew <mark@markdrew.net>
2017-03-31 21:39:08 -0500

    Added Dokka comment and gradle config

--- src/main/kotlin/hello/Greeter.kt
+++ src/main/kotlin/hello/Greeter.kt
@@ -1,5 +1,10 @@
 package hello
 
+/**
+ * Builds greetings
+ *
+ * @param format a function that given a name, produces a greeting
+ */
 class Greeter(val format: (String) -> String) {
 
     fun greeting(name: String) = format(name)
